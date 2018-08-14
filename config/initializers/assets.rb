58671ba99c6f41e648f78813e761f7bcda55ef96
Kian Yang <kianyang@gmail.com>
2018-08-14 17:49:01 +0800

    Fixed responsive view and sidebar issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( admin.js admin.css material/bootstrap-material-design.min.js material/perfect-scrollbar.jquery.min.js material/chartist.min.js material/bootstrap-notify.js material/material-dashboard.min.js material/demo.js)
